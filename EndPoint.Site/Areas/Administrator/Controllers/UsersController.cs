c1cba9f1ccdf651f1ec0efd32e912aff949dca5a
Eshizaki <eshizaki49@gmail.com>
2024-03-14 17:02:49 +0330

    feat: admin template added

---
+++ EndPoint.Site/Areas/Administrator/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.Site.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- EndPoint.Site/Program.cs
+++ EndPoint.Site/Program.cs
@@ -29,6 +29,9 @@ namespace EndPoint.Site
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
